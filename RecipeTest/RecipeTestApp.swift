8ad9ffca4115e6b56edfc967ebe0591093b94ebf
Rishita Meharishi <rmeharishi@mail.sfsu.edu>
2025-04-21 14:01:16 -0700

    Initial Commit

---
+++ RecipeTest/RecipeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTestApp.swift
+//  RecipeTest
+//
+//  Created by Rishita Meharishi on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
